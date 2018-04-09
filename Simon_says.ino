06e5cfac8785afaebcfb60fea4e21c627a0ec81d
lolo31r <lorenzo.schaaf@gmail.com>
2018-04-09 19:17:32 +0200

    Add files via upload

--- Simon_says.ino
+++ Simon_says.ino
@@ -1,42 +1,4 @@
 
-/*
-
-www.kreativekiste.de 
-
- SparkFun Inventor's Kit
- Example sketch 16
- Spark Fun Electronics
- Oct. 7, 2014
-
- Simon Says is a memory game. Start the game by pressing one of the four buttons. When a button lights up, 
- press the button, repeating the sequence. The sequence will get longer and longer. The game is won after 
- 13 rounds.
-
- Generates random sequence, plays music, and displays button lights.
-
- Simon tones from Wikipedia
- - A (red, upper left) - 440Hz - 2.272ms - 1.136ms pulse
- - a (green, upper right, an octave higher than A) - 880Hz - 1.136ms,
- 0.568ms pulse
- - D (blue, lower left, a perfect fourth higher than the upper left) 
- 587.33Hz - 1.702ms - 0.851ms pulse
- - G (yellow, lower right, a perfect fourth higher than the lower left) - 
- 784Hz - 1.276ms - 0.638ms pulse
-
- Simon Says game originally written in C for the PIC16F88.
- Ported for the ATmega168, then ATmega328, then Arduino 1.0.
- Fixes and cleanup by Joshua Neal <joshua[at]trochotron.com>
-
- This sketch was written by SparkFun Electronics,
- with lots of help from the Arduino community.
- This code is completely free for any use.
- Visit http://www.arduino.cc to learn about the Arduino.
- */
-
-/*************************************************
-* Public Constants
-*************************************************/
-
 #define NOTE_B0 31
 #define NOTE_C1 33
 #define NOTE_CS1 35
@@ -221,8 +183,8 @@ void loop()
     if (play_memory() == true) 
       play_winner(); // Player won, play winner tones
     else 
-      //play_loser(); // Player lost, play loser tones
-      #include <LiquidCrystal.h>
+      play_loser(); // Player lost, play loser tones
+      /*#include <LiquidCrystal.h>
       #include <Wire.h>
 
 LiquidCrystal lcd(12, 11, 10, 5, 4, 3, 2);
@@ -233,7 +195,7 @@ void setup()
   lcd.print("hello, world!");
 }
 
-void loop() {}
+void loop() {}*/
 }
   }
 
